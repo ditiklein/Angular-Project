e0f2ee80e6d5f5cb518e41b8b67a5f52068e4942
ditiklein <diti3809@gmail.com>
2025-03-07 15:41:14 +0200

    with-build

--- Final-Project/src/app/app.config.ts
+++ Final-Project/src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { routes } from './app.routes';
 import { tokenInterceptor } from '../intersceptor/token.interceptor';
+import { routes } from './app.routes';
+console.log(routes); // בדיקה אם מוגדר
 
 export const appConfig: ApplicationConfig = {
   providers: [
