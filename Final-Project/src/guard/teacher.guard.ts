53cc255094c9eab670abe7d266a4b3425a475f30
ditiklein <diti3809@gmail.com>
2025-03-07 16:17:32 +0200

    power

--- Final-Project/src/guard/teacher.guard.ts
+++ Final-Project/src/guard/teacher.guard.ts
@@ -2,7 +2,6 @@ import { CanActivateFn } from '@angular/router';
 
 export const teacherGuard: CanActivateFn = (route, state) => {
   if(sessionStorage.getItem('role')=='teacher')
-  return true;
-
-  return true;
+    return true;
+  return false;
 };
